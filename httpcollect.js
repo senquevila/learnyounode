71a0027b18092ccc57aab47801e12e17484b7895
senquevila <senquevila@gmail.com>
2014-01-22 15:28:07 -0600

    HTTP Collect con la version corta en comentarizada

--- httpcollect.js
+++ httpcollect.js
@@ -27,10 +27,13 @@ var bl = require('bl')
 http.get(process.argv[2], function (response) {
   response.pipe(bl(function (err, data) {
     if (err)
-      return console.error(data)
-    data = data.toString()
-    console.log(data.length)
-    console.log(data)
-  }))
+      return console.error(data);
+      
+    data = data.toString();
+    
+    console.log(data.length);
+    console.log(data);
+  }));
+  
 });
 */
